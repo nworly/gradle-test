7e963a616bc3ae73cf9814b3fe3b9377e2195f58
Elizaveta Zabolotnaia <zabolotnaya.ea@gmail.com>
2022-04-25 18:47:22 +0300

    init commit

---
+++ service/src/main/java/MyService.java
@@ -0,0 +1,21 @@
+import java.util.UUID;
+
+public class MyService {
+    private DbSettings dbSetting = new DbSettings("name", "password");
+    private String name = "myService";
+    private Db db = new Db(dbSetting);
+
+    public String getName() {
+        return name;
+    }
+
+    public MyEntity setMyEntity(MyEntity myEntity) {
+        myEntity.setId(UUID.randomUUID());
+        db.setMyEntity(myEntity);
+        return myEntity;
+    }
+
+    public MyEntity getMyEntity() {
+        return db.getMyEntity();
+    }
+}
